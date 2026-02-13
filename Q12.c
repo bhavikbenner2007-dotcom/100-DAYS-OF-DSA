54890279c7b1410fc2212a57fb0e5bb8402f7f1a
BHAVIK <bhavikbenner2007@gmail.com>
2026-02-13 23:44:00 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int a[50][50]; /* simple fixed-size array; works for sizes up to 50 */
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if (m != n) {
+        printf("Not a Symmetric Matrix\n");
+        return 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                printf("Not a Symmetric Matrix\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix\n");
+    return 0;
+}
